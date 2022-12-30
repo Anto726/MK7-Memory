eedb661661b88a24bb4d7f6fdd67c2d1d7cba2fa
Anto726 <44021141+Anto726@users.noreply.github.com>
2022-12-31 00:55:33 +0100

    Update Kart::VehicleMove

--- include/Kart/VehicleMove.hpp
+++ include/Kart/VehicleMove.hpp
@@ -6,7 +6,9 @@ namespace Kart
 	class VehicleMove : public VehicleControlAI
 	{
 	public:
-		u8 m_0xC28[1516];
+		u8 m_0xC28[1028];
+		s32 m_invincibility_frames;
+		u8 m_0x1030[484];
 	};
 	static_assert(sizeof(VehicleMove) == 0x1214);
 }
