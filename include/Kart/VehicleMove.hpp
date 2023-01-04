c19a015c8892e99efd11f3c3c1a9bb8915302603
Anto726 <44021141+Anto726@users.noreply.github.com>
2023-01-04 17:20:10 +0100

    Update Kart::VehicleMove

--- include/Kart/VehicleMove.hpp
+++ include/Kart/VehicleMove.hpp
@@ -6,7 +6,15 @@ namespace Kart
 	class VehicleMove : public VehicleControlAI
 	{
 	public:
-		u8 m_0xC28[1028];
+		u8 m_0xC28[972];
+		s32 m_star_frames;
+		s32 m_ink_frames;
+		s32 m_anim_frames;
+		s32 m_thunder_frames;
+		s32 m_press_frames;
+		u8 m_0x1008[12];
+		float m_press_size;
+		u8 m_0x1018[20];
 		s32 m_invincibility_frames;
 		u8 m_0x1030[484];
 	};
