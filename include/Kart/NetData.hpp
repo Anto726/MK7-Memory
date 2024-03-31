417daed23e1e242bd87414842bff0f0813fa7247
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-03-31 18:42:32 +0200

    Update Kart::NetData

--- include/Kart/NetData.hpp
+++ include/Kart/NetData.hpp
@@ -6,7 +6,9 @@ namespace Kart
 {
     struct NetData
     {
-        u8 gap_0x0[0x10];
+        u8 gap_0x0[0x4];
+        u32 kart_data;
+        u8 gap_0x8[0x8];
         sead::Vector3<s16> position;
         sead::Vector3<s16> velocity;
         sead::Vector3<s16> angular_velocity;
