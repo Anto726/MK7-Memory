740bf09d27b8685abbe184ff275749349f7fc49a
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-07-23 11:29:43 +0200

    Update Kart::VehicleMove::StatusFlags

--- include/Kart/VehicleMove.hpp
+++ include/Kart/VehicleMove.hpp
@@ -9,10 +9,38 @@ namespace Kart
 	public:
 		struct StatusFlags
 		{
-			u32 gap_0 : 22;
-			u32 killer : 1;
-			u32 gap_24 : 1;
-			u32 tail : 1;
+			u32 _0x1 : 1;
+			u32 _0x2 : 1;
+			u32 _0x4 : 1;
+			u32 _0x8 : 1;
+			u32 _0x10 : 1;
+			u32 jugem_recover : 1; // 0x20
+			u32 jugem_recover_ai : 1; // 0x40
+			u32 _0x80 : 1;
+			u32 _0x100 : 1;
+			u32 _0x200 : 1;
+			u32 _0x400 : 1;
+			u32 _0x800 : 1;
+			u32 _0x1000 : 1;
+			u32 _0x2000 : 1;
+			u32 _0x4000 : 1;
+			u32 _0x8000 : 1;
+			u32 accident_1 : 1; // 0x10000
+			u32 _0x20000 : 1;
+			u32 _0x40000 : 1;
+			u32 _0x80000 : 1;
+			u32 _0x100000 : 1;
+			u32 _0x200000 : 1;
+			u32 killer : 1; // 0x400000
+			u32 _0x800000 : 1;
+			u32 tail : 1; // 0x1000000
+			u32 battle_restart : 1; // 0x2000000
+			u32 _0x4000000 : 1;
+			u32 _0x8000000 : 1;
+			u32 _0x10000000 : 1;
+			u32 _0x20000000 : 1;
+			u32 _0x40000000 : 1;
+			u32 _0x80000000 : 1;
 		};
 		static_assert(sizeof(StatusFlags) == 0x4);
 
