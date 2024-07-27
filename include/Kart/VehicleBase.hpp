a6f286e3e93ed182998a1f8a659f9e06048a4cf1
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-07-27 10:57:38 +0200

    Update Vehicle stuff

--- include/Kart/VehicleBase.hpp
+++ include/Kart/VehicleBase.hpp
@@ -7,18 +7,20 @@ namespace Kart
 	class VehicleBase : public Rigid
 	{
 	public:
-		u8 gap_0x74[4];
+		u8 gap_0x74[0x4];
 		Director *m_director;
-		u8 gap_0x7C[8];
+		u8 gap_0x7C[0x8];
 		s32 m_player_id;
-		u8 gap_0x88[16];
+		u8 gap_0x88[0x10];
 		bool m_is_master;
-		u8 gap_0x99[4];
+		u8 gap_0x99[0x4];
 		bool m_is_net_send;
 		bool m_is_net_recv;
-		u8 gap_0x9F[4];
+		u8 gap_0x9F[0x4];
 		bool m_is_intangible;
-		u8 gap_0xA4[56];
+		u8 gap_0xA4[0x2];
+		bool m_is_fake_goal;
+		u8 gap_0xA7[0x35];
 	};
 	static_assert(sizeof(VehicleBase) == 0xDC);
 }
--- include/Kart/VehicleMove.hpp
+++ include/Kart/VehicleMove.hpp
@@ -9,44 +9,46 @@ namespace Kart
 	public:
 		struct StatusFlags
 		{
-			u32 _0x1 : 1;
-			u32 _0x2 : 1;
-			u32 _0x4 : 1;
-			u32 _0x8 : 1;
-			u32 _0x10 : 1;
-			u32 jugem_recover : 1; // 0x20
-			u32 jugem_recover_ai : 1; // 0x40
-			u32 _0x80 : 1;
-			u32 _0x100 : 1;
-			u32 _0x200 : 1;
-			u32 _0x400 : 1;
-			u32 _0x800 : 1;
-			u32 _0x1000 : 1;
-			u32 _0x2000 : 1;
-			u32 _0x4000 : 1;
-			u32 _0x8000 : 1;
-			u32 accident_1 : 1; // 0x10000
-			u32 _0x20000 : 1;
-			u32 _0x40000 : 1;
-			u32 _0x80000 : 1;
-			u32 _0x100000 : 1;
-			u32 _0x200000 : 1;
-			u32 killer : 1; // 0x400000
-			u32 _0x800000 : 1;
-			u32 tail : 1; // 0x1000000
-			u32 battle_restart : 1; // 0x2000000
-			u32 _0x4000000 : 1;
-			u32 _0x8000000 : 1;
-			u32 _0x10000000 : 1;
-			u32 _0x20000000 : 1;
-			u32 _0x40000000 : 1;
-			u32 _0x80000000 : 1;
+			u32 _0x1 				: 1;
+			u32 _0x2 				: 1;
+			u32 _0x4 				: 1;
+			u32 _0x8 				: 1;
+			u32 _0x10 				: 1;
+			u32 jugem_recover 		: 1; // 0x20
+			u32 jugem_recover_ai 	: 1; // 0x40
+			u32 _0x80 				: 1;
+			u32 _0x100 				: 1;
+			u32 _0x200 				: 1;
+			u32 _0x400 				: 1;
+			u32 _0x800 				: 1;
+			u32 _0x1000 			: 1;
+			u32 _0x2000 			: 1;
+			u32 _0x4000 			: 1;
+			u32 _0x8000 			: 1;
+			u32 accident_1 			: 1; // 0x10000
+			u32 _0x20000 			: 1;
+			u32 _0x40000 			: 1;
+			u32 _0x80000 			: 1;
+			u32 _0x100000 			: 1;
+			u32 _0x200000 			: 1;
+			u32 killer 				: 1; // 0x400000
+			u32 _0x800000 			: 1;
+			u32 tail 				: 1; // 0x1000000
+			u32 battle_restart 		: 1; // 0x2000000
+			u32 _0x4000000 			: 1;
+			u32 vanish_start 		: 1; // 0x8000000
+			u32 _0x10000000 		: 1;
+			u32 _0x20000000 		: 1;
+			u32 _0x40000000 		: 1;
+			u32 _0x80000000 		: 1;
 		};
 		static_assert(sizeof(StatusFlags) == 0x4);
 
 		u8 gap_0xC28[0x8];
 		StatusFlags m_status_flags;
-		u8 gap_0xC34[0x2D4];
+		u8 gap_0xC34[0x174];
+		s32 m_dokan_warp;
+		u8 gap_0xDAC[0x15C];
 		float m_miniturbo_charge;
 		u8 gap_0xF0C[0x20];
 		float m_forward_speed;
