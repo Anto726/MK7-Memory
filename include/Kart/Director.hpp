1d50eb3d6dbc36aa7e5a60b86b7cfd009ffa5443
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-07-30 15:22:54 +0200

    Update Kart::Director

--- include/Kart/Director.hpp
+++ include/Kart/Director.hpp
@@ -2,15 +2,16 @@
 
 #include "../forward.hpp"
 
-#include <container/seadBuffer.h>
+#include <container/seadPtrArray.h>
 
 namespace Kart
 {
     class Director
     {
     public:
-        u8 gap_0x0[40];
-        sead::Buffer<Unit *> m_units;
+        u8 gap_0x0[0x24];
+        sead::PtrArray<Unit> m_units;
+        u8 gap_0x30[0x170];
     };
-    static_assert(sizeof(Director) == 0x30);
+    static_assert(sizeof(Director) == 0x1A0);
 }
