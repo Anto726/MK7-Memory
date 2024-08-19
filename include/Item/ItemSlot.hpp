270c4c33440dd3fff204bcc0d1a2c68e94a77ad6
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-08-19 11:17:26 +0200

    Update stuff

--- include/Item/ItemSlot.hpp
+++ include/Item/ItemSlot.hpp
@@ -8,6 +8,11 @@ namespace Item
 	class ItemSlot : public Object::Actor
 	{
 	public:
+		enum ObserverStatus : u8
+        {
+            ObserverStatus_Stock = 3,
+        };
+
 		Kart::InfoProxy *m_info_proxy;
 		Util::TStateObserver<Item::ItemSlot> m_state_observer;
 		u8 gap_0x2C[0x1C];
--- include/Util/TStateObserver.hpp
+++ include/Util/TStateObserver.hpp
@@ -8,16 +8,11 @@ namespace Util
     class TStateObserver
     {
     public:
-        enum class Status : u8
-        {
-            Stock = 3,
-        };
-
         virtual ~TStateObserver() {}; // 1
         void resetState() {}; // 2
         void executeState() {}; // 3
 
-        Status m_status_current;
+        u8 m_status_current;
         u8 gap_0x5[0x1B];
     };
     static_assert(sizeof(TStateObserver<void>) == 0x20);
