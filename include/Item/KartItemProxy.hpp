a353ae4f7dd559ddd2758416748e5b2b019485f6
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-10-09 11:48:43 +0200

    Update Item::KartItemProxy

--- include/Item/KartItemProxy.hpp
+++ include/Item/KartItemProxy.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
-#include "../forward.hpp"
+#include "ItemDirector.hpp"
+#include "../System/RootSystem.hpp"
 
 namespace Item
 {
     class KartItemProxy
     {
     public:
+        KartItemProxy(s32 index) : m_kart_item(System::g_root_system->get_item_director()->m_kart_items.at(index)) {}
+
         KartItem *m_kart_item;
     };
     static_assert(sizeof(KartItemProxy) == 0x4);
