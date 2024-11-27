dd05c13c104c61e3202793d567cfe76049a63192
Anto726 <44021141+Anto726@users.noreply.github.com>
2024-11-27 21:57:31 +0100

    Add CheckPoint stuff

---
+++ include/Field/Accessor/CheckPoint.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Base.hpp"
+
+#include "../Entry/CheckPoint.hpp"
+
+#include "../../System/RootSystem.hpp"
+#include "../CourseInfo.hpp"
+#include "../FieldDirector.hpp"
+
+namespace Field
+{
+    class MapdataCheckPointAccessor : public MapdataAccessorBase<MapdataCheckPoint, MapdataCheckPoint::SData> {};
+    static_assert(sizeof(MapdataCheckPointAccessor) == 0x18);
+
+    static auto GetCheckPointAccessor()
+    {
+        return System::g_root_system->get_field_director()->m_course_info->m_check_point_accessor;
+    }
+}
