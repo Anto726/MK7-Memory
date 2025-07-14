58dc08ce423889735511f8a2c4b9d01edf0d0095
PabloMK7 <hackyglitch2@gmail.com>
2025-07-14 22:56:08 +0200

    BaseRacePage research

--- template/Sequence/BaseRacePage.hpp
+++ template/Sequence/BaseRacePage.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
 #include "../forward.hpp"
+#include "RaceSys/CRaceMode.hpp"
 
 #include <container/seadPtrArray.h>
 
 namespace Sequence
 {
-    class BaseRacePage
-    {
+    /START_CLASS/NAME@BaseRacePage/SIZE@0x31FC/
     public:
-        u8 gap_0x0[0x26C];
-        RaceSys::CRaceMode m_race_mode;
-        u8 gap_0x278[0x2F20];
-        sead::PtrArray<UI::BgRaceMapBombControl> m_bg_race_map_bomb_controls;
-        u8 gap_0x31A4[0x58];
-    };
-    static_assert(sizeof(BaseRacePage) == 0x31FC);
+        /M/RaceSys::CRaceMode m_race_mode/0xC/0x26C/
+        /M/u32 m_result_bar_point_timer/0x4/0x2E34/
+        /M/u32 m_result_bar_point_sound_timer/0x4/0x2E38/
+        /M/u32 m_result_bar_point_increment/0x4/0x2E48/
+        /M/u32 m_result_bar_point_interval/0x4/0x2E4C/
+        /M/u32 m_result_bar_point_sound_interval/0x4/0x2E50/
+        /M/sead::PtrArray<UI::BgRaceMapBombControl> m_bg_race_map_bomb_controls/0xC/0x3198/
+    /END/
 }
