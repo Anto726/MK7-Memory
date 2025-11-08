442268d2cfb650653e8cd512c48117d05b0dfc7e
Bsquo <suyikaki37@gmail.com>
2025-11-08 01:00:47 +0000

    Add the rest of the archive IDs

--- template/System/EArchiveID.hpp
+++ template/System/EArchiveID.hpp
@@ -6,7 +6,17 @@ BEGIN_NAMESPACE(System)
 {
     enum class EArchiveID : u32
     {
-        THANKYOU_3D = 0x9,
-        ROMFS_ROOT = 0xB,
+        COMMON,                     // Common.szs
+        RACE_COMMON,                // RaceCommon.szs
+        COURSE,                     // Course directory
+        UI_COMMON,                  // UI/common.szs
+        UI_RACE,                    // UI/race.szs
+        UI_MENU,                    // UI/menu.szs
+        MENU_3D,                    // Menu3D.szs
+        UI_TROPHY,                  // UI/trophy.szs
+        UI_ENDING_AND_THANKYOU,     // UI/ending.szs and UI/thankyou.szs
+        THANKYOU_3D,                // Thankyou3D.szs
+        ARCHIVE_0xA,
+        ROMFS_ROOT
     };
 }
