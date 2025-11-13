94b564c6164c5e3915074f930c7cb8edf69652c9
Anto726 <44021141+Anto726@users.noreply.github.com>
2025-11-13 13:20:36 +0100

    Update Object::Actor3DMdl

--- template/Object/Actor3DMdl.hpp
+++ template/Object/Actor3DMdl.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "../types.hpp"
 #include "Actor.hpp"
+
+#include <math/seadMatrix.h>
 
 BEGIN_NAMESPACE(Object)
 {
@@ -9,6 +10,10 @@ BEGIN_NAMESPACE(Object)
     public:
         virtual void *getDTIClassInfo() const { return {}; }; // 0
 		virtual void *getDTIClass() const { return {}; }; // 1
-    
+
+        /M/sead::Vector3f m_0x8/0xC/0x8/
+        /M/sead::Matrix34f m_0x14/0x30/0x14/
+        /M/bool m_0x44/0x1/0x44/
+        /M/Render::DrawMdl *m_draw_mdl/0x4/0x54/
     /END/
 }
