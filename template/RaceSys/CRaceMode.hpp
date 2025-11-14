9218393bb4ee5d19d9c37a87b42bb909f4c055f7
Anto726 <44021141+Anto726@users.noreply.github.com>
2025-11-14 11:27:11 +0100

    Update CRaceMode

--- template/RaceSys/CRaceMode.hpp
+++ template/RaceSys/CRaceMode.hpp
@@ -2,12 +2,23 @@
 
 #include "../types.hpp"
 
+#include "ERacePlayMode.hpp"
 #include "ERaceRuleMode.hpp"
 
 BEGIN_NAMESPACE(RaceSys)
 {
     /START_CLASS/NAME@CRaceMode/SIZE@0xC/
     public:
-        /M/ERaceRuleMode m_race_rule_mode/0x4/0x4/
+        // unknown name
+        enum class RaceType : s32
+        {
+            Coin,
+            Balloon,
+            Race,
+        };
+
+        /M/ERacePlayMode m_play_mode/0x4/0x0/
+        /M/ERaceRuleMode m_rule_mode/0x4/0x4/
+        /M/RaceType m_type/0x4/0x8/
     /END/
 }
---
+++ template/RaceSys/ERacePlayMode.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../types.hpp"
+
+BEGIN_NAMESPACE(RaceSys)
+{
+    enum class ERacePlayMode : u32
+    {
+    };
+}
