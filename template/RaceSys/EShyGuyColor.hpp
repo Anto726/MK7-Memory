d4ced66c5012ea0a837418698b70865475748401
Anto726 <44021141+Anto726@users.noreply.github.com>
2025-11-14 11:13:46 +0100

    Add RaceSys::EShyGuyColor

---
+++ template/RaceSys/EShyGuyColor.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../types.hpp"
+
+BEGIN_NAMESPACE(RaceSys)
+{
+    enum class EShyGuyColor : u8
+    {
+        Red,
+        Blue,
+        Yellow,
+        Green,
+        LightBlue,
+        Pink,
+        Black,
+        White,
+        MAX,
+    };
+}
