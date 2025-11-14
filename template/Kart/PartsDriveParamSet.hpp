d311fb437f6b2cbc47aa381d1d2c4bbab2579c70
Anto726 <44021141+Anto726@users.noreply.github.com>
2025-11-14 11:19:36 +0100

    Add PartsDriveParamSet

---
+++ template/Kart/PartsDriveParamSet.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "../types.hpp"
+
+BEGIN_NAMESPACE(Kart)
+{
+    /START_STRUCT/NAME@PartsDriveParamSet/SIZE@0xB4/
+        /M/float m_mass_stopped/0x4/0x0/
+        /M/float m_mass/0x4/0x4/
+        /M/float m_mass_dash/0x4/0x8/
+        /M/float m_speed_max_forward_land/0x4/0xC/
+        /M/float m_speed_max_forward_land_max_coins/0x4/0x10/
+        /M/float m_speed_max_forward_sea/0x4/0x14/
+        /M/float m_speed_max_forward_sea_max_coins/0x4/0x18/
+        /M/float m_speed_max_forward_air_neutral/0x4/0x1C/
+        /M/float m_speed_max_forward_air_downward/0x4/0x20/
+        /M/float m_ground_lift_air/0x4/0x24/
+        /M/float m_acceleration_limit_forward_land/0x4/0x28/
+        /M/float m_speed_kf_forward_land/0x4/0x2C/
+        /M/float m_handling_drive_land/0x4/0x30/
+        /M/float m_handling_drift_land/0x4/0x34/
+        /M/float m_handling_auto_drift_land/0x4/0x38/
+        /M/float m_handling_drive_sea/0x4/0x3C/
+        /M/float m_handling_drift_sea/0x4/0x40/
+        /M/float m_handling_auto_drift_sea/0x4/0x44/
+        /M/float m_handling_slide_sea/0x4/0x48/
+        /M/float m_handling_roll_air/0x4/0x4C/
+        /M/float m_handling_move_air/0x4/0x50/
+        /M/float m_brake_dirt_light/0x4/0x54/
+        /M/float m_brake_dirt_medium/0x4/0x58/
+        /M/float m_brake_dirt_heavy/0x4/0x5C/
+        /M/float m_brake_sand_light/0x4/0x60/
+        /M/float m_brake_sand_medium/0x4/0x64/
+        /M/float m_brake_sand_heavy/0x4/0x68/
+        /M/float m_brake_ice_light/0x4/0x6C/
+        /M/float m_brake_ice_medium/0x4/0x70/
+        /M/float m_brake_ice_heavy/0x4/0x74/
+        /M/float m_slip_dirt_light/0x4/0x78/
+        /M/float m_slip_dirt_medium/0x4/0x7C/
+        /M/float m_slip_dirt_heavy/0x4/0x80/
+        /M/float m_slip_sand_light/0x4/0x84/
+        /M/float m_slip_sand_medium/0x4/0x88/
+        /M/float m_slip_sand_heavy/0x4/0x8C/
+        /M/float m_slip_ice_light/0x4/0x90/
+        /M/float m_slip_ice_medium/0x4/0x94/
+        /M/float m_slip_ice_heavy/0x4/0x98/
+        /M/float m_miniturbo_boost_amount_level1/0x4/0x9C/
+        /M/float m_miniturbo_boost_amount_level2/0x4/0xA0/
+        /M/float m_tire_feeling_1/0x4/0xA4/
+        /M/float m_tire_feeling_2/0x4/0xA8/
+        /M/float m_tire_feeling_3/0x4/0xAC/
+        /M/float m_drift_start/0x4/0xB0/
+    /END/
+}
