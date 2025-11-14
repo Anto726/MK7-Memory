f1b0fdc6c46f14d93bcc8f7336da6292d7419c91
Anto726 <44021141+Anto726@users.noreply.github.com>
2025-11-14 11:07:20 +0100

    Update ItemSlot

--- template/Item/ItemSlot.hpp
+++ template/Item/ItemSlot.hpp
@@ -2,6 +2,7 @@
 
 #include "../types.hpp"
 
+#include "../Net/NetworkItemSlotMgr.hpp"
 #include "../Object/Actor.hpp"
 #include "../Util/TStateObserver.hpp"
 
@@ -15,6 +16,13 @@ BEGIN_NAMESPACE(Item)
         };
 
 		/M/Kart::InfoProxy *m_info_proxy/0x4/0x8/
-		/M/Util::TStateObserver<Item::ItemSlot> m_state_observer/0x20/0xC/
+		/M/Util::TStateObserver<ItemSlot> m_state_observer/0x20/0xC/
+		/M/eItemSlot m_item_id/0x1/0x2C/
+		/M/s32 m_current_id/0x4/0x30/
+		/M/s32 m_next_id/0x4/0x34/
+		/M/float m_ratio/0x4/0x38/
+		/M/float m_0x3C_speed/0x4/0x3C/
+		/M/s32 m_spinning_frames/0x4/0x40/
+		/M/Net::NetworkItemSlotMgr::EStockItemState m_stock_item_state/0x4/0x44/
 	/END/
 }
--- template/Net/NetworkItemSlotMgr.hpp
+++ template/Net/NetworkItemSlotMgr.hpp
@@ -6,6 +6,16 @@ BEGIN_NAMESPACE(Net)
 {
     /START_CLASS/NAME@NetworkItemSlotMgr/SIZE@0x1/
     public:
+        enum class EEquipItemState : u32
+        {
+            
+        };
+
+        enum class EStockItemState : u32
+        {
+            
+        };
+
         /START_STRUCT/NAME@Buffer/SIZE@0x1/
         /END/
     /END/
