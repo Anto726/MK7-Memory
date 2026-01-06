1906b947fa8e8f76d9867ffe20aa7200ce2cef10
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-01-07 00:05:39 +0100

    Fix TIndLinkList

--- template/Util/TIndLinkList.hpp
+++ template/Util/TIndLinkList.hpp
@@ -9,9 +9,9 @@ BEGIN_NAMESPACE(Util)
     template <class T>
     /START_CLASS/NAME@TIndLinkList/SIZE@0x10/SIZEOF@TIndLinkList<void>/VTABLE@True/
     public:
-        virtual bool append(TLinkNode<T> *) {}; // 0
-        virtual bool prepend(TLinkNode<T> *) {}; // 1
-        virtual bool release(TLinkNode<T> *) {}; // 2
+        virtual bool append(TLinkNode<T> *) { return {}; }; // 0
+        virtual bool prepend(TLinkNode<T> *) { return {}; }; // 1
+        virtual bool release(TLinkNode<T> *) { return {}; }; // 2
 
         /M/TLinkNode<T> *m_head/0x4/0x4/
         /M/TLinkNode<T> *m_tail/0x4/0x8/
