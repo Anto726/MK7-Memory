cee3020f80272f047366f8ead9fa277ebd270357
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-01-08 15:03:18 +0100

    Update Net::NetworkBuffer

--- template/Net/NetworkBuffer.hpp
+++ template/Net/NetworkBuffer.hpp
@@ -7,24 +7,32 @@ BEGIN_NAMESPACE(Net)
 {
     /START_CLASS/NAME@NetworkBuffer/SIZE@0x10/
     public:
-        void Clear()
+		NetworkBuffer(eNetworkBufferType type, u8 *data, size_t size) : m_type(type), m_data(data), m_size(size), m_current(0)
 		{
-			m_current = 0;
-			memset(m_data, 0, m_size);
+			memset(data, 0, size);
+			m_current = 0; // NOTE: does it again for some reason?
 		}
 
-        void Add(void* new_data, size_t new_data_size)
+		void add(void *new_data, size_t new_data_size)
 		{
 			if (m_current + new_data_size > m_size)
 				return;
+
 			memcpy(m_data + m_current, new_data, new_data_size);
 			m_current += new_data_size;
 		}
 
-        void Set(void* new_data, size_t new_data_size)
+        void clear()
+		{
+			memset(m_data, 0, m_size);
+			m_current = 0;
+		}
+
+        void set(void *new_data, size_t new_data_size)
 		{
 			if (new_data_size > m_size)
 				return;
+
 			memcpy(m_data, new_data, new_data_size);
 			m_current = new_data_size;
 		}
