87f190d5337763ff9a2a84cd2da61cc0202dfd2d
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-01-21 16:06:12 +0100

    Add EAcdType

--- template/Kart/VehicleReact.hpp
+++ template/Kart/VehicleReact.hpp
@@ -8,10 +8,34 @@ BEGIN_NAMESPACE(Kart)
 	public:
 		enum class EAcdType : u8
 		{
+			PipeFall = 0x0,
+			Fireball = 0x1,
+			Flipper = 0x2,
+			NoSounds = 0x3,
+			Spin1 = 0x4,
+			Spin2 = 0x5,
+			FirePillar = 0x6,
+			KoopaZapper = 0x7,
+			Thunder = 0x8,
+			_0x9 = 0x9,
+			CrashDir = 0xA,
+			CrashDirBig = 0xB,
+			DokanWarp = 0xC,
+			CrashLR = 0xD,
+			CrashLRBig = 0xE,
+			CrashFwd = 0xF,
+			CrashHop = 0x10,
+			Freeze = 0x11,
+			Dummy = 0x12,
 			MAX = 0x13,
 		};
 		
 		enum class ECallType : u32
+		{
+
+		};
+
+		enum class ESpinType
 		{
 
 		};
