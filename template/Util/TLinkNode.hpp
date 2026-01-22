59638168e9be9c1b6726bfbf9f60931046b28981
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-01-22 17:01:15 +0100

    Update TLinkNode

--- template/Util/TLinkNode.hpp
+++ template/Util/TLinkNode.hpp
@@ -5,9 +5,10 @@
 BEGIN_NAMESPACE(Util)
 {
     template <class T>
-    /START_CLASS/NAME@TLinkNode/SIZE@0x8/SIZEOF@TLinkNode<void>/
+    /START_CLASS/NAME@TLinkNode/SIZE@0xC/SIZEOF@TLinkNode<void>/
     public:
         /M/TLinkNode<T> *m_prev/0x4/0x0/
         /M/TLinkNode<T> *m_next/0x4/0x4/
+        /M/T *m_value/0x4/0x8/
     /END/
 }
