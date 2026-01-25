3252325657efa643894e403a83aa7300fbcaf421
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-01-25 13:46:54 +0100

    Update ERaceModeFlag
    Co-authored-by: B_squo <88517417+bsquo@users.noreply.github.com>

--- template/RaceSys/ERaceModeFlag.hpp
+++ template/RaceSys/ERaceModeFlag.hpp
@@ -6,7 +6,16 @@ BEGIN_NAMESPACE(RaceSys)
 {
     struct ERaceModeFlag
     {
-        u32 race : 1;
+        u32
+            race : 1,
+            race_or_battle : 1,
+            race_or_course_preview : 1,
+            multiplayer_or_online : 1,
+            replay_grand_prix : 1,
+            time_trials : 1,
+            live_view : 1,
+            course_has_patch : 1, // v1.1+ (Wuhu Loop / Maka Wuhu / DK Jungle / GBA Bowser Castle 1)
+            not_battle : 1;
     };
     static_assert(sizeof(ERaceModeFlag) == 0x4);
 }
