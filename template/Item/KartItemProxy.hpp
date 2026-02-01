0eda9a668fc83f8af5bfdd87133f2e452f846f98
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-02-01 14:43:17 +0100

    Update KartItemProxy

--- template/Item/KartItemProxy.hpp
+++ template/Item/KartItemProxy.hpp
@@ -10,6 +10,21 @@ BEGIN_NAMESPACE(Item)
     public:
         KartItemProxy(s32 index) : m_kart_item(GetDirector()->m_kart_items.at(index)) {}
 
+        auto getCurrentID_Slot() const { return m_kart_item->m_item_slot->m_current_id; }
+        auto getEquipItem() const { return m_kart_item->m_equip_item; }
+        auto getEquipNum() const { return m_kart_item->m_equip_item_amount; }
+        auto getNextID_Slot() const { return m_kart_item->m_item_slot->m_next_id; }
+        auto getRatio_Slot() const { return m_kart_item->m_item_slot->m_ratio; }
+        auto getStockItem() const { return m_kart_item->m_stock_item; }
+        auto isEmptyTrig_Slot() const { return m_kart_item->m_item_slot->isEmptyTrig(); }
+        auto isEquip() const { return m_kart_item->m_equip_type == Item::eEquipType::Hang || m_kart_item->m_equip_type == Item::eEquipType::Multi; }
+        auto isEquip_Hang() const { return m_kart_item->m_equip_type == Item::eEquipType::Hang; }
+        auto isEquip_Multi() const { return m_kart_item->m_equip_type == Item::eEquipType::Multi; }
+        auto isSpin_Slot() const { return m_kart_item->m_item_slot->m_spinning_frames >= 0; }
+        auto isStock() const { return m_kart_item->m_item_slot->isStock(); }
+        auto isStopTrig_Slot() const { return m_kart_item->m_item_slot->isStopTrig(); }
+        auto isValidQuickStop_Slot() const { return m_kart_item->m_item_slot->isValidQuickStop(); }
+
         /M/KartItem *m_kart_item/0x4/0x0/
     /END/
 }
