88b5cfe40f72526d8a18aa12fc3e174fb81ad981
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-02-01 16:42:56 +0100

    Add comment

--- template/RaceSys/RaceInfo/Get.hpp
+++ template/RaceSys/RaceInfo/Get.hpp
@@ -7,6 +7,7 @@
 
 #include "../../System/RootSystem.hpp"
 
+// NOTE: it's here to prevent circular dependency issues
 BEGIN_NAMESPACE(RaceSys)
 {
     static CRaceInfo *GetRaceInfo() { return &System::g_root_system->get_race_director()->m_mode_manager->m_race_info; }
