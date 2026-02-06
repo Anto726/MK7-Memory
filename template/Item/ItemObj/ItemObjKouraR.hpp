0018f87c67564400d9d5db65a636929be0bd3ce8
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-02-06 22:55:20 +0100

    Update ItemObjKouraR

--- template/Item/ItemObj/ItemObjKouraR.hpp
+++ template/Item/ItemObj/ItemObjKouraR.hpp
@@ -7,5 +7,8 @@ BEGIN_NAMESPACE(Item)
     /START_CLASS/NAME@ItemObjKouraR/SIZE@0x2C4/BASE@ItemObjKouraG/BSIZE@0x23C/
     public:
         /M/RaceSys::LapRankChecker *m_lap_rank_checker/0x4/0x288/
+        /M/s32 m_target_player_id/0x4/0x28C/
+        /M/Kart::Director *m_kart_director/0x4/0x290/
+        /M/Kart::Vehicle *m_target_vehicle/0x4/0x294/
     /END/
 }
