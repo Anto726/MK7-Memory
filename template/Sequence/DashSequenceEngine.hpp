16f0994962a6fd51d82521ed0c53a6f016f4fed5
Bsquo <suyikaki37@gmail.com>
2026-02-21 11:31:20 +0000

    SequenceEngine docs

--- template/Sequence/DashSequenceEngine.hpp
+++ template/Sequence/DashSequenceEngine.hpp
@@ -9,6 +9,7 @@ BEGIN_NAMESPACE(Sequence)
     /START_CLASS/NAME@DashSequenceEngine/SIZE@0x16F5C/BASE@SequenceEngine/BSIZE@0xAC/VTABLE@True/
     public:
         /M/MenuData *m_menu_data/0x4/0xC0/
+        /M/bool m_exit_app/0x1/0xD0/
         /M/UI::MessageIDConverter *m_message_id_converter/0x4/0xD8/
     /END/
 }
--- template/Sequence/SequenceEngine.hpp
+++ template/Sequence/SequenceEngine.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "../Object/ActorEngine.hpp"
+#include "SceneSequence.hpp"
+#include "../UI/ManipulatorManager.hpp"
+#include "../Sequence/Page.hpp"
+#include "../System/EArchiveID.hpp"
+
+#include <container/seadPtrArray.h>
 
 BEGIN_NAMESPACE(Sequence)
 {
     /START_CLASS/NAME@SequenceEngine/SIZE@0xAC/BASE@Object::ActorEngine/BSIZE@0x8/
+    public:
+        /M/SceneSequence *m_brs_scene_sequence/0x4/0x8/ // Scene sequence associated to the top-most component ("Root") in the .brs. Aka rootSequence
+        /M/SceneSequence *m_bss_scene_sequence/0x4/0xc/ // Scene sequence associated to the top-most component of the current scene's .bss
+        /M/UI::ManipulatorManager m_manipulator_manager/0x40/0x40/
+        /M/System::EArchiveID m_archive_id/0x4/0x80/    // Archive ID associated with the current scene
+        /M/sead::PtrArray<Sequence::Page> m_active_pages/0xC/0x88/
     /END/
 }
