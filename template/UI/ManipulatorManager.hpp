d4b3bd3e384ea059e92241188c081caff936cb48
Bsquo <suyikaki37@gmail.com>
2026-02-21 11:07:01 +0000

    A few ManipulatorManager docs

--- template/UI/ManipulatorManager.hpp
+++ template/UI/ManipulatorManager.hpp
@@ -18,11 +18,11 @@ BEGIN_NAMESPACE(UI)
         /M/bool m_disable_inputs/0x1/0x2/
         /M/Manipulator *m_0x4/0x4/0x4/
         /M/sead::FixedPtrArray<Manipulator, 3> m_manipulator_array/0x18/0x8/
-        /M/sead::Delegate<Sequence::BaseRacePage> *m_calc_input_delegate_ptr/0x4/0x20/    // Sequence::BaseRacePage::calcInput
+        /M/sead::Delegate<Sequence::BaseRacePage> *m_calc_input_delegate_ptr/0x4/0x20/    // `Sequence::BaseRacePage::calcInput`
         /M/sead::Vector2f m_touch_input/0x8/0x24/
-        /M/u8 m_0x2c/0x1/0x2c/
+        /M/bool m_is_touch_handler_slide_out/0x1/0x2c/  // Assumption
         /M/sead::Vector2f m_stick/0x8/0x30/
-        /M/f32 m_0x38/0x4/0x38/
-        /M/f32 m_0x3c/0x4/0x3c/
+        /M/f32 m_0x38/0x4/0x38/ // See `UI::ManipulatorManager::calcStick`
+        /M/f32 m_stick_x_min/0x4/0x3c/
     /END/
 }
