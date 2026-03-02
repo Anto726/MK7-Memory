4550c57ae596dcbf6de3c4ec570dde45245f3720
Bsquo <suyikaki37@gmail.com>
2026-03-02 00:21:17 +0000

    Add initTime() prototype

--- template/Sequence/BaseRacePage.hpp
+++ template/Sequence/BaseRacePage.hpp
@@ -12,6 +12,7 @@ BEGIN_NAMESPACE(Sequence)
     /START_CLASS/NAME@BaseRacePage/SIZE@0x31FC/BASE@BasePage/BSIZE@0x26C/VTABLE@True/
     public:
         BaseRacePage();
+        void initTime();
 
         /M/RaceSys::CRaceMode m_race_mode/0xC/0x26C/
         /M/UI::BgRaceMapControl *m_bg_race_map_control_1/0x4/0x29C/
