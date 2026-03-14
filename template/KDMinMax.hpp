870dd27acfc36566124ff12dba9429430ca82006
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-03-14 11:10:08 +0100

    Update KDMinMax

--- template/KDMinMax.hpp
+++ template/KDMinMax.hpp
@@ -9,6 +9,12 @@ public:
     void convToWorld(KDAttT *);
     void extend(sead::Vector3f *);
 
+    inline void reset()
+    {
+        m_min = sead::Vector3f::zero;
+        m_max = sead::Vector3f::zero;
+    }
+
     /M/sead::Vector3f m_min/0xC/0x0/
     /M/sead::Vector3f m_max/0xC/0xC/
 /END/
