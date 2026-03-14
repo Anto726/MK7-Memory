765415f5dcc6a355af9b19f19d621e069c841296
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-03-14 11:09:30 +0100

    Update KDAttT

--- template/KDAttT.hpp
+++ template/KDAttT.hpp
@@ -6,14 +6,28 @@
 
 BEGIN_GLOBAL_NAMESPACE
 
-class KDAttT
-{
+/START_CLASS/NAME@KDAttT/SIZE@0x24/
 public:
-    sead::Vector3f m_forward;
-    sead::Vector3f m_right;
-    sead::Vector3f m_up;
-};
-static_assert(sizeof(KDAttT) == 0x24);
+    KDAttT() {};
+
+    inline void reset()
+    {
+        m_forward = sead::Vector3f::zero;
+        m_right = sead::Vector3f::zero;
+        m_up = sead::Vector3f::zero;
+    }
+
+    inline void init()
+    {
+        m_forward = sead::Vector3f::ex;
+        m_right = sead::Vector3f::ey;
+        m_up = sead::Vector3f::ez;
+    }
+
+    // TODO: make sure names aren't mixed up
+    /M/sead::Vector3f m_forward/0xC/0x0/
+    /M/sead::Vector3f m_right/0xC/0xC/
+    /M/sead::Vector3f m_up/0xC/0x18/
+/END/
 
 END_GLOBAL_NAMESPACE
-
