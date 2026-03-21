c97479f3c02bad3715d1544278fce4ae5f013f39
Bsquo <suyikaki37@gmail.com>
2026-03-21 16:17:08 +0000

    char16 -> char16_t in Debug print funcs

--- template/Debug/print.hpp
+++ template/Debug/print.hpp
@@ -8,9 +8,9 @@
 BEGIN_NAMESPACE(Debug)
 {
     // 0x001b3608
-    void PrintTextToTopScreenWChar(s32 pos_x, s32 pos_y, const char16 *string, ...);
+    void PrintTextToTopScreenWChar(s32 pos_x, s32 pos_y, const char16_t *string, ...);
     // 0x001fb3b0
-    void PrintTextToBottomScreenWChar(s32 pos_x, s32 pos_y, const char16 *string, ...);
+    void PrintTextToBottomScreenWChar(s32 pos_x, s32 pos_y, const char16_t *string, ...);
     // 0x001fb340
     void PrintTextToBottomScreenChar(s32 pos_x, s32 pos_y, const char *string, ...);
 }
