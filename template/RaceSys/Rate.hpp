2abd1a6cc3ff499be5911ffc39989c400b7f26d7
Bsquo <suyikaki37@gmail.com>
2026-03-27 21:15:43 +0000

    Update Rate

--- template/RaceSys/Rate.hpp
+++ template/RaceSys/Rate.hpp
@@ -2,11 +2,17 @@
 
 #include "../types.hpp"
 
+#define RATE_MIN     1
+#define RATE_DEFAULT 1000
+#define RATE_MAX     99999
+
 BEGIN_NAMESPACE(RaceSys)
 {
-    struct Rate
-    {
-        u32 value;
-    };
-    static_assert(sizeof(Rate) == 0x4);
+    /START_STRUCT/NAME@Rate/SIZE@0x4/
+        f32 winTo(const Rate &, s32) const;
+        f32 loseTo(const Rate &, s32) const;
+        static f32 calcPointDiff(u32, s32); // Name is made up. 0x00257188 (EUR DLP)
+
+        /M/u32 value/0x4/0x0/
+    /END/
 }
