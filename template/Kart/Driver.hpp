6f21cb121bf4dda87547c62cb968324fddcd1a56
PabloMK7 <hackyglitch2@gmail.com>
2026-03-31 18:23:49 +0200

    Fix comment on driver goal motion

--- template/Kart/Driver.hpp
+++ template/Kart/Driver.hpp
@@ -6,6 +6,6 @@ BEGIN_NAMESPACE(Kart){
     /START_CLASS/NAME@Driver/SIZE@0xE8/
     public:
         /M/s32 m_driver_id/0x4/0x8/
-        /M/u8 m_goal_motion_state/0x4/0x54/ // 0 -> Lose, 1 -> Win, 2 -> Idle (default)
+        /M/u8 m_goal_motion_state/0x4/0x54/ // 0 -> Win, 1 -> Lose, 2 -> Idle (default)
     /END/
 }
