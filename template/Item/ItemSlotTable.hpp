7e3a680ca588f5d5291f870a643646d16eca4060
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-04-05 12:28:59 +0200

    Add `Item::ItemSlotTable`

---
+++ template/Item/ItemSlotTable.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../forward.hpp"
+#include "../types.hpp"
+
+#include "../Object/Actor.hpp"
+
+#include <container/seadPtrArray.h>
+
+BEGIN_NAMESPACE(Item)
+{
+    // unused TDirectorBase?
+	/START_CLASS/NAME@ItemSlotTable/SIZE@0xA8/BASE@Object::Actor/BSIZE@0x8/
+	public:
+        /M/sead::FixedPtrArray<CSVUtil::CSVParam, 28> m_param_array/0x7C/0x28/
+        /M/void *m_0xA4/0x4/0xA4/ // RaceSys::ModeManagerBase *m_mode_manager?
+	/END/
+}
