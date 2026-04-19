0db97ae33059bf6acedb613a258da604e80d4732
Bsquo <suyikaki37@gmail.com>
2026-04-19 23:31:36 +0100

    Separate the input buffer in KDPadBinData

--- template/System/KDPadBinData.hpp
+++ template/System/KDPadBinData.hpp
@@ -6,6 +6,12 @@ BEGIN_NAMESPACE(System)
 {
     // Raw ghost input data
     /START_STRUCT/NAME@KDPadBinData/SIZE@0x27d8/
-        /M/u8 m_data[0x27d8]/0x27d8/0x0/
+        // Name is made up
+        /START_STRUCT/NAME@InputBuffer/SIZE@0x27d4/
+            /M/u8 m_data[0x27d4]/0x27d4/0x0/
+        /END/
+        
+        /M/InputBuffer m_input_buffer/0x27d4/0x0/
+        /M/u32 m_checksum/0x4/0x27d4/
     /END/
 }
--- template/System/KDPadData.hpp
+++ template/System/KDPadData.hpp
@@ -3,9 +3,6 @@
 #include "../forward.hpp"
 #include "../types.hpp"
 
-#include <container/seadPtrArray.h>
-#include <math/seadVector.h>
-
 BEGIN_NAMESPACE(System)
 {
     /START_CLASS/NAME@KDPadDataBase/SIZE@0x1C/
