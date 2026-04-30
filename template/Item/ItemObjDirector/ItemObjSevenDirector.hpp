906c8b27863c80d5f9d5fd0d105578a4737bdf2d
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-04-30 11:57:41 +0200

    Update ItemObjSevenDirector

--- template/Item/ItemObjDirector/ItemObjSevenDirector.hpp
+++ template/Item/ItemObjDirector/ItemObjSevenDirector.hpp
@@ -6,5 +6,6 @@ BEGIN_NAMESPACE(Item)
 {
     /START_CLASS/NAME@ItemObjSevenDirector/SIZE@0xE4/BASE@ItemObjDirectorBase/BSIZE@0xBC/
     public:
+        /M/sead::FixedPtrArray<ItemObjBase, 7> m_seven_array/0x28/0xBC/
     /END/
 }
