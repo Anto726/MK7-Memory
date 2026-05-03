02d59ed4e9d6551382c4f18277716cbe3d20c48b
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-03 11:39:14 +0200

    Update FieldDirector

--- template/Field/FieldDirector.hpp
+++ template/Field/FieldDirector.hpp
@@ -6,7 +6,7 @@
 
 BEGIN_NAMESPACE(Field)
 {
-    /START_CLASS/NAME@FieldDirector/SIZE@0x2AC/BASE@Object::ActorEngine/BSIZE@0x8/
+    /START_CLASS/NAME@FieldDirector/SIZE@0x2AC/BASE@Object::TDirectorList<Object::Actor3DMdlList, Object::TLinkSocket<Object::Actor, Object::Actor>>/BSIZE@0x28/
     public:
         /M/CourseInfo *m_course_info/0x4/0x38/
     /END/
