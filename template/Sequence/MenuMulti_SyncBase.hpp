fc6c7b110696b6f4ba2c401f7d89d73a52c273b6
Bsquo <suyikaki37@gmail.com>
2026-05-04 15:44:31 +0100

    Fix typo

--- template/Sequence/MenuMulti_SyncBase.hpp
+++ template/Sequence/MenuMulti_SyncBase.hpp
@@ -5,7 +5,7 @@
 BEGIN_NAMESPACE(Sequence)
 {
     template <typename T>
-    class MenuMulti_SyncBase: public: BaseMenuPage {
+    class MenuMulti_SyncBase: public BaseMenuPage {
     public:
         virtual void onPagePreStep();
         virtual void onPageEnter();
