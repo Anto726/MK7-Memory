1c8893091962cdadcc548fc5b9b6f5c439b068fa
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-10 14:48:00 +0200

    Add point and path base

---
+++ template/Field/Entry/base.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../../types.hpp"
+
+BEGIN_NAMESPACE(Field)
+{
+    /START_CLASS/NAME@MapdataPointDataBase/TEMPLATE@template <typename T>/SIZE@0x4/SIZEOF@MapdataPointDataBase<void>/
+    public:
+        /M/T *m_data/0x4/0x0/
+    /END/
+
+    /START_CLASS/NAME@MapdataPathDataBase/TEMPLATE@template <typename T>/SIZE@0x8/SIZEOF@MapdataPathDataBase<void>/VTABLE@True/
+    public:
+        virtual int getStartPoint();
+        virtual int getPointNum();
+        virtual int getEndPoint();
+        virtual bool isInclude(unsigned int);
+
+        /M/T *m_data/0x4/0x4/
+    /END/
+}
