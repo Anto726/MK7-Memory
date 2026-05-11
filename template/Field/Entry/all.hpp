122b3b745b8d2068ebec82638b0c82fdec4bc1b3
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-11 17:56:35 +0200

    Add all points include

---
+++ template/Field/Entry/all.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "AreaPoint.hpp"
+#include "CannonPoint.hpp"
+#include "CheckPoint.hpp"
+#include "EnemyPoint.hpp"
+#include "ItemPoint.hpp"
+#include "JugemPoint.hpp"
+#include "MissionPoint.hpp"
+#include "StartPoint.hpp"
