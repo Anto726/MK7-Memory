4a8af1d005ef102d037e4ad964c900240f8f7701
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-11 17:53:18 +0200

    Add fields

--- template/Field/Entry/StartPoint.hpp
+++ template/Field/Entry/StartPoint.hpp
@@ -8,6 +8,8 @@ BEGIN_NAMESPACE(Field)
 {
     /START_STRUCT/NAME@MapdataStartPointData/SIZE@0x1C/
         /M/sead::Vector3f position/0xC/0x0/
+        /M/sead::Vector3f rotation/0xC/0xC/
+        /M/s16 kart_index/0x2/0x18/
     /END/
 
     /START_CLASS/NAME@MapdataStartPoint/SIZE@0x2C/BASE@MapdataPointDataBase<MapdataStartPointData>/BSIZE@0x4/
