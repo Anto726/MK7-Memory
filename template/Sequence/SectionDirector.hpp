a65b0c699d7b7e0cfc26bcc163ec50a8a1a28a32
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-11 18:28:34 +0200

    Add initial SectionDirector

---
+++ template/Sequence/SectionDirector.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../types.hpp"
+
+#include "../Object/TDirectorArray.hpp"
+#include "../Object/TLinkSocket.hpp"
+
+#include "Section.hpp"
+
+BEGIN_NAMESPACE(Sequence)
+{
+    /START_CLASS/NAME@SectionDirector/SIZE@0x38/BASE@Object::TDirectorArray<Section, Object::TLinkSocket<Object::Actor, Object::Actor>>/BSIZE@0x28/
+    public:
+        
+    /END/
+}
