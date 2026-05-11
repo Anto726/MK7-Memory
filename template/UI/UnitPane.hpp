bfbf8573df965cb850a23ba92e8104aaf08c7b54
Bsquo <suyikaki37@gmail.com>
2026-05-11 11:20:20 +0100

    Add UnitPicPane

--- template/UI/UnitPane.hpp
+++ template/UI/UnitPane.hpp
@@ -6,5 +6,7 @@ BEGIN_NAMESPACE(UI)
 {
     /START_CLASS/NAME@UnitPane/SIZE@0x34/
     public:
+        /START_STRUCT/NAME@CopyArg/SIZE@0x38/
+        /END/
     /END/
 }
---
+++ template/UI/UnitPicPane.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "UnitPane.hpp"
+
+BEGIN_NAMESPACE(UI)
+{
+    /START_CLASS/NAME@UnitPicPane/SIZE@0x48/BASE@UnitPane/BSIZE@0x34/
+    public:
+    /END/
+}
