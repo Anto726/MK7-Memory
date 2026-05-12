24a2f8d6f10de33dd75bc5d0aea1fd7bc4afe318
Bsquo <suyikaki37@gmail.com>
2026-05-12 22:40:19 +0100

    Add MiiIntroControl and RaceDemoTextControl

---
+++ template/UI/MiiIntroControl.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "BaseFastControl.hpp"
+
+BEGIN_NAMESPACE(UI)
+{
+    /START_CLASS/NAME@MiiIntroControl/SIZE@0x7C/BASE@BaseFastControl/BSIZE@0x7C/VTABLE@True/
+    public:
+        /START_STRUCT/NAME@AnimationDefine/SIZE@0x18/BASE@ControlAnimator::AnimationDefine/BSIZE@0x18/VTABLE@True/
+            virtual void defineAnimation(); // 0
+        /END/
+
+        /START_STRUCT/NAME@CreateArg/SIZE@0x90/BASE@VisualControl::CreateArg/BSIZE@0x90/VTABLE@True/
+            virtual const ControlAnimator::AnimationDefine *getAnimationDefine() const; // 3
+        /END/
+
+        virtual Object::DTIClassInfo *getDTIClassInfo() const;
+        virtual Object::DTIClassInfo *getDTIClass() const;
+        virtual ~MiiIntroControl();
+        
+        void set(s32, s32);
+    /END/
+}
---
+++ template/UI/RaceDemoTextControl.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "BaseFastControl.hpp"
+
+BEGIN_NAMESPACE(UI)
+{
+    /START_CLASS/NAME@RaceDemoTextControl/SIZE@0x7C/BASE@BaseFastControl/BSIZE@0x7C/VTABLE@True/
+    public:
+        /START_STRUCT/NAME@AnimationDefine/SIZE@0x18/BASE@ControlAnimator::AnimationDefine/BSIZE@0x18/VTABLE@True/
+            virtual void defineAnimation(); // 0
+        /END/
+
+        /START_STRUCT/NAME@CreateArg/SIZE@0x90/BASE@VisualControl::CreateArg/BSIZE@0x90/VTABLE@True/
+            virtual const ControlAnimator::AnimationDefine *getAnimationDefine() const; // 3
+        /END/
+
+        virtual Object::DTIClassInfo *getDTIClassInfo() const;
+        virtual Object::DTIClassInfo *getDTIClass() const;
+        virtual ~RaceDemoTextControl();
+        virtual void onReset();
+    /END/
+}
