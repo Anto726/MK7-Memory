0c6e25a54bb179dcd0f9c39ac93f7225a8c4ecb6
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-16 11:21:15 +0200

    Add initial GarageDirector

---
+++ template/Menu3D/GarageDirector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../types.hpp"
+
+#include "../Object/TDirectorList.hpp"
+#include "../Object/TLinkSocket.hpp"
+
+BEGIN_NAMESPACE(Menu3D)
+{
+    /START_CLASS/NAME@GarageDirector/SIZE@0xCC/BASE@Object::TDirectorList<Object::Actor, Object::TLinkSocket<Object::Actor, Object::Actor>>/BSIZE@0x28/
+	public:
+	/END/
+}
