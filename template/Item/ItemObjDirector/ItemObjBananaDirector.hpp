9464ca84b319cbadea062b1ed428d750e0e8c09f
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-18 13:42:43 +0200

    Update ItemObjBananaDirector

--- template/Item/ItemObjDirector/ItemObjBananaDirector.hpp
+++ template/Item/ItemObjDirector/ItemObjBananaDirector.hpp
@@ -2,9 +2,16 @@
 
 #include "ItemObjDirectorBase.hpp"
 
+#include <container/seadRingBuffer.h>
+#include <math/seadMatrix.h>
+
 BEGIN_NAMESPACE(Item)
 {
     /START_CLASS/NAME@ItemObjBananaDirector/SIZE@0x13C/BASE@ItemObjDirectorBase/BSIZE@0xBC/
     public:
+        // TODO: needs to be fixed in the Python script
+        using RingBuffers_t = sead::RingBuffer<sead::Matrix34f> [8];
+
+        /U/RingBuffers_t/0x80/0xBC/
     /END/
 }
